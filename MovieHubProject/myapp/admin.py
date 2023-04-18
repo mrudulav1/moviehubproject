b3af87dad07559cc04c52ca38e222d4a628d1d7a
mrudula <mrudulavaji11@gmail.com>
2023-04-18 22:27:23 +0530

    creating movie hub project with two models

---
+++ MovieHubProject/myapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from myapp.models import Genres,Movies
+admin.site.register(Genres)
+admin.site.register(Movies)
---
+++ MovieHubProject/myapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Genres(models.Model):
+    genre=models.CharField(max_length=120,unique=True)
+    is_active=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.genre
+    
+class Movies(models.Model):
+    name=models.CharField(max_length=250,unique=True)
+    genre=models.ManyToManyField(Genres,null=True)
+    year=models.CharField(max_length=200)
+    options={
+        ('malayalm','malayalm'),
+        ('telungu','telungu'),
+        ('tamil','tamil'),
+        ('english','english')
+    }
+    language=models.CharField(max_length=200,choices=options,default='malayalm')
+    runtime=models.FloatField()
+    poster_image=models.ImageField(upload_to="images",null=True,blank=True)
+    description=models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.name
